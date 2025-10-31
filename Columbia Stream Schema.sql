ab8da6e7ac6be464d742b681c77d31cb0a81b858
RupeetK <rupeet.kaur@columbia.edu>
2025-10-30 20:27:38 -0400

    Update Columbia Stream Schema.sql

--- Columbia Stream Schema.sql
+++ Columbia Stream Schema.sql
@@ -1,20 +1,25 @@
-create table if not exists Courses
+DROP TABLE IF EXISTS CourseInstructors;
+DROP TABLE IF EXISTS Videos;
+DROP TABLE IF EXISTS CourseOfferings;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Courses;
+
+create table Courses
 (
     course_id   varchar(50)  not null
         primary key,
-    course_code varchar(50)  not null,
-    course_name varchar(200) not null,
-    constraint course_code
-        unique (course_code)
+    course_name varchar(200) not null
+
 );
 
-create table if not exists CourseOfferings
+create table CourseOfferings
 (
     offering_id int auto_increment
         primary key,
     course_id   varchar(50)             not null,
     semester    enum ('Spring', 'Fall') not null,
     year        int                     not null,
+    section     int                     not null,
     constraint CourseOfferings_ibfk_1
         foreign key (course_id) references Courses (course_id)
             on delete cascade
@@ -23,7 +28,7 @@ create table if not exists CourseOfferings
 create index course_id
     on CourseOfferings (course_id);
 
-create table if not exists Users
+create table Users
 (
     uni      varchar(50)                 not null
         primary key,
@@ -34,7 +39,7 @@ create table if not exists Users
         unique (email)
 );
 
-create table if not exists CourseInstructors
+create table  CourseInstructors
 (
     offering_id int         not null,
     prof_uni    varchar(50) not null,
@@ -50,7 +55,7 @@ create table if not exists CourseInstructors
 create index prof_uni
     on CourseInstructors (prof_uni);
 
-create table if not exists Videos
+create table Videos
 (
     video_id    int auto_increment
         primary key,
@@ -73,5 +78,22 @@ create index offering_id
 create index prof_uni
     on Videos (prof_uni);
 
-select * from Users;
+insert into Courses values('COMSW4153','Cloud Computing');
+insert into Courses values('COMSW4111','Introduction to Databases');
+
+insert into CourseOfferings(course_id, semester, year,section) values('COMSW4153','Fall',2025,1);
+insert into CourseOfferings(course_id, semester, year,section) values('COMSW4153','Fall',2025,2);
+insert into CourseOfferings(course_id, semester, year,section) values('COMSW4111','Fall',2025,1);
+
+insert into Users values('abc9','faculty','abc9@columbia.edu','qwerty123');
+insert into Users values('def9','faculty','def9@columbia.edu','qwerty124');
+insert into Users values('abc4','student','abc4@columbia.edu','qwerty12');
+insert into Users values('fgh5','student','fgh5@columbia.edu','qwerty1');
+
+
+insert into CourseInstructors values(1,'abc9');
+insert into CourseInstructors values(2,'def9');
+insert into CourseInstructors values(3,'def9');
+
+select * from Courses;
 
